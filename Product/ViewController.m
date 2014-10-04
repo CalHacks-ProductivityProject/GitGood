ea3e070717ecaef0181958e579be0f6de7c85fd1
Stephen Meriwether <smeriwether714@gmail.com>
2014-10-04 01:28:50 -0700

    Initial Commit

---
+++ Product/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Product
+//
+//  Created by Stephen Meriwether on 10/4/14.
+//  Copyright (c) 2014 CalHacksProductivity. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
