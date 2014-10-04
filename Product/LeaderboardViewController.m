b4fa04f6cd99eda9eee030e9a938aef5fd9275eb
Stephen Meriwether <smeriwether714@gmail.com>
2014-10-04 06:42:07 -0700

    Added leaderboard controller

---
+++ Product/LeaderboardViewController.m
@@ -0,0 +1,39 @@
+//
+//  LeaderboardViewController.m
+//  Product
+//
+//  Created by Stephen Meriwether on 10/4/14.
+//  Copyright (c) 2014 CalHacksProductivity. All rights reserved.
+//
+
+#import "LeaderboardViewController.h"
+
+@interface LeaderboardViewController ()
+
+@end
+
+@implementation LeaderboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self.navigationController setTitle:@"Leaderboards"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
